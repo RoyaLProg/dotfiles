5b9c6ccbf6305f67047fe61dd0094f24f8372305
Camille CAMBIUM <royalfr@protonmail.com>
2024-03-02 14:33:49 +0100

    UPDATE : nvim/lua/core/plugin_config/lsp_config.lua
    adding keybinds and servers to the lsp config

--- nvim/lua/core/plugin_config/lsp_config.lua
+++ nvim/lua/core/plugin_config/lsp_config.lua
@@ -5,7 +5,27 @@ require("mason-lspconfig").setup({
 	ensure_installed = {"lua_ls", "clangd", "tsserver"}
 })
 
+local on_attach = function ( )
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+	vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+
+	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+	vim.keymap.set('n', '<leader>ca', vim.lsp.buf.codeaction, {})
+end
+
 -- init language servers
-require("lspconfig").lua_ls.setup {}
-require("lspconfig").clangd.setup {}
-require("lspconfig").tsserver.setup {}
+require("lspconfig").lua_ls.setup {
+	on_attach = on_attach
+}
+require("lspconfig").clangd.setup  {
+	on_attach = on_attach
+}
+
+require("lspconfig").tsserver.setup  {
+	on_attach = on_attach
+}
+
+
+
