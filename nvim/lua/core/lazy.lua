9d552404f3f35b614c748e7cb59ec80fd037db85
Camille CAMBIUM <royalfr@protonmail.com>
2024-08-24 16:25:57 +0200

    NEW : nvim noice, mini.ai
    welcome noice and mini.ai to my vim config

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -81,4 +81,20 @@ require("lazy").setup({
 		cmd = "Silicon",
 	},
 	'folke/todo-comments.nvim',
+	{"echasnovski/mini.nvim", version = false},
+	{
+	  "folke/noice.nvim",
+	  event = "VeryLazy",
+	  opts = {
+	    -- add any options here
+	  },
+	  dependencies = {
+	    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+	    "MunifTanjim/nui.nvim",
+	    -- OPTIONAL:
+	    --   `nvim-notify` is only needed, if you want to use the notification view.
+	    --   If not available, we use `mini` as the fallback
+	    "rcarriga/nvim-notify",
+	    }
+	}
 })
