76bab27ad541c890a9fa461ca3c82597c7cffebc
Camille CAMBIUM <royalfr@protonmail.com>
2025-11-24 10:43:25 +0100

    fix(nvim): fixing hover borders

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -51,9 +51,6 @@ require("lazy").setup({
 			{
 				"L3MON4D3/LuaSnip",
 				-- follow latest release.
-				version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-				-- install jsregexp (optional!).
-				build = "make install_jsregexp",
 				dependencies = {
 					'saadparwaiz1/cmp_luasnip',
 					'rafamadriz/friendly-snippets'
@@ -72,9 +69,6 @@ require("lazy").setup({
 	{
 	  "folke/noice.nvim",
 	  event = "VeryLazy",
-	  opts = {
-	    -- add any options here
-	  },
 	  dependencies = {
 	    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 	    "MunifTanjim/nui.nvim",
