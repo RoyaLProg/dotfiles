f240e669306f8ce9bf3ad0369b65d282cbf04798
Camille CAMBIUM <camille.cambium@protonmail.com>
2026-01-23 21:36:44 +0100

    fix(fish): better config function

--- fish/custom_functions/config.fish
+++ fish/custom_functions/config.fish
@@ -1,12 +1,21 @@
 #simple function to configure dotfiles from everywhere
 
-function config --wraps="nvim $HOME/.config/" --description "alias config=nvim $HOME/.config[/dir]"
+function config --wraps="cd $HOME/.config/" --description "alias config=nvim $HOME/.config[/dir]"
 
+	set OLD_PWD $PWD
+	set CONFIG_FOLDER $HOME/.config
 	if test "$(echo $argv | wc -w)" -gt 1
 		echo "config [config repertory]"
-	else if test -n "$argv"
-		nvim /home/royal/.config/$argv
-	else
-		nvim /home/royal/.config
+		return 0
 	end
+
+	if ! test -d $CONFIG_FOLDER/$argv
+		echo "config: folder $argv does not exist under $CONFIG_FOLDER"
+		return 1
+	end
+
+	cd $CONFIG_FOLDER/$argv
+	nvim .
+	cd $OLD_PWD
+	set -e OLD_PWD
 end
